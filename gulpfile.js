77c3ae861038df68c0c4dfae1c96164f3edc02c2
Gean Saturno <cin_gsaturno@uolinc.com>
2017-05-30 14:38:37 -0300

    delpoy de client

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,56 @@
-var gulp    = require('gulp');
-var bs      = require('browser-sync');
+// jshint esversion:6
 
-gulp.task('server', function(){
+let g               = require('gulp');
+let browserSync     = require('browser-sync');
+let gplugins        = require('gulp-load-plugins')();
 
-    bs.init({
+g.task('server', () => {
+
+    browserSync.init({
+        server: {
+            baseDir: "src/client/"
+        }
+    });
+
+    g.watch('client/**/*', browserSync.reload);
+});
+
+g.task('server-build', () => {
+
+    browserSync.init({
         server: {
             baseDir: "client/"
         }
     });
 
-    gulp.watch('client/**/*', bs.reload);
+    g.watch('client/**/*', browserSync.reload);
+});
+
+g.task('clean', () => {
+    return g
+        .src('client/')
+        .pipe(gplugins.clean());
+});
+
+g.task('copy', ['clean'], () => {
+    return g
+        .src('src/client/**/*')
+        .pipe(g.dest('client/'));
+});
+
+g.task('babel', ['copy'], () => {
+    return g.src('client/js/app/**/*.js')
+        .pipe(gplugins.babel({presets : ['es2015'] }))
+        .pipe(g.dest('client/js/app/'));
+});
+
+g.task('usemin', ['babel'], () => {
+
+    g.src('client/**/*.html')
+        .pipe(gplugins.usemin({
+            js      : [gplugins.uglify],
+            css     : [gplugins.cssmin],
+            html    : [gplugins.htmlmin({collapseWhitespace: true})]
+        }))
+        .pipe(g.dest('client/'));
 });
